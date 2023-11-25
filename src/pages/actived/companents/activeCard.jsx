e1e915f47b104dffece68c6f121a9c46d4a2f33e
Abbos1506 <abbosomonboyev686@gmail.com>
2023-11-25 15:25:28 +0500

    image click change

--- src/pages/actived/companents/activeCard.jsx
+++ src/pages/actived/companents/activeCard.jsx
@@ -1,10 +1,14 @@
-import CardInfoComp from "./cardInfo";
 import CardSlider from "./cardslidercomp";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import { ShoppingCartOutlined, UserSwitchOutlined, RetweetOutlined } from "@ant-design/icons";
+import BrendLogo from "../../../assets/image/brend-logo.png"
+import ModalComp from "../../../companents/modal";
+
 
 const ActCardComp = () => {
 
     const [data, setData] = useState([]);
+    const [id, setId] = useState("https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg")
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products?limit=5", {
@@ -20,21 +24,82 @@ const ActCardComp = () => {
       })
   }, []);
 
+  console.log(data)
+
     return <div className="actcard__comp">
         <div className="card__slider">
             <CardSlider/>
         </div>
         <div className="comp__img">
+            <div className="card__image">
+              <img src={id} alt="/" />
+            </div>
+        </div>
+        <div className="card__image__info">
+        <div className="card__info__page">
+        <div className="info__color">
+        <div className="info__title">
+            <p>Цвет:</p>
+        </div>
+        <div className="info__img">
             {
-                data.slice(0,1).map(item => (
-                    <div className="card__image">
-                        <img src={item.image} alt="item image" />
+                data.slice(0,3).map(item => (
+                    <div className="color__card" key={item.id}  onClick={() => setId(item.image)}>
+                        <img src={item.image} alt="card image" />
                     </div>
                 ))
             }
         </div>
-        <div className="card__info__page">
-          <CardInfoComp/>
+    </div>
+        </div>
+        <div className="card__description">
+            {
+                data.slice(0,1).map(item => (
+                    <div className="description__card" key={item.id}>
+                        <div className="description__title">
+                            <p>Состав: {item.category}</p>
+                        </div>
+                        <div className="description__size">
+                            <h2>Размеры:</h2>
+                            <select name="select" id="1">
+                                <option value="38">38</option>
+                                <option value="39">39</option>
+                                <option value="40">40</option>
+                                <option value="41">41</option>
+                            </select>
+                        </div>
+                        <div className="description__links">
+                            <div className="description__delivery">
+                                <i><ShoppingCartOutlined /></i>
+                                <p>Бесплатная доставка</p>
+                            </div>
+                            <div className="description__delivery">
+                                <i><UserSwitchOutlined /></i>
+                                <p>Примерка</p>
+                            </div>
+                            <div className="description__delivery">
+                                <i><RetweetOutlined /></i>
+                                <p>Возврат товара</p>
+                            </div>
+                        </div>
+                        <div className="description__brend">
+                            <div className="brend__title">
+                                <p>Бренд</p>
+                            </div>
+                            <div className="brend__image">
+                                <img src={BrendLogo} alt="brend logo" />
+                            </div>
+                        </div>
+                        <div className="description__title">
+                            <h2>{Math.floor(item.price)} 000 UZS</h2>
+                        </div>
+                        <div className="description__btn">
+                            <ModalComp/>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
         </div>
         <div className="card__plan">
           <div className="card__plan__title">
